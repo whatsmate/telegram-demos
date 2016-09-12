e3f62fc14573f366a9cce84c1ff1f7b1e94618d9
WhatsMate <whatsmate@gmail.com>
2016-09-12 16:03:49 +0800

    add a script to send a message to a single recipient

---
+++ python/send-single-telegram-text.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import requests
+
+# TODO: When you have your own Client ID and secret, put down their values here:
+clientId = "FREE_TRIAL_ACCOUNT"
+clientSecret = "PUBLIC_SECRET"
+
+jsonBody = {
+    'number': '12025550108',  # FIXME: Put down your registered number here
+    'message': 'Have a nice day! Loving you:)'  # FIXME: Customize your own message  
+}
+
+headers = {
+    'X-WM-CLIENT-ID': clientId, 
+    'X-WM-CLIENT-SECRET': clientSecret
+}
+
+r = requests.post('http://api.whatsmate.net/v1/telegram/single/message/0',
+    headers=headers,
+    json=jsonBody)
+
+print("Status code: " + str(r.status_code))
+print("Response: " + str(r.content))
+
