f864a0e681821504439a702234186de525412104
WhatsMate <whatsmate@gmail.com>
2016-10-14 12:51:26 +0800

    send photo to a single recipient

---
+++ bash/send-photo-single.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# TODO: Put down your own Client ID and secret here:
+CLIENT_ID="YOUR_OWN_CLIENT_ID_HERE"
+CLIENT_SECRET="YOUR_OWN_SECRET_HERE"
+
+# TODO: Customize the following 3 files:
+number="12025550108"
+caption="Lovely Girl" 
+base64_image=`base64 -w 0 ../assets/cute-girl.jpg`
+
+
+cat > /tmp/jsonbody.txt << _EOM_
+  {
+    "number": "$number",
+    "caption": "$caption",
+    "image": "$base64_image"
+  }
+_EOM_
+
+
+curl --show-error -X POST \
+     -H "X-WM-CLIENT-ID: $CLIENT_ID" \
+     -H "X-WM-CLIENT-SECRET: $CLIENT_SECRET" \
+     -H "Content-Type: application/json" \
+     --data-binary @/tmp/jsonbody.txt  \
+     http://api.whatsmate.net/v1/telegram/single/photo/binary/0
+
+echo -e "\n=== END OF DEMO ==="
